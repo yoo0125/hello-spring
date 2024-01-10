47dddbfc8de5b498fdd0b757f39e4ced1636631a
yoo0125 <yooo0125@naver.com>
2024-01-10 19:00:38 +0900

    섹션 3 회원 관리 예제 - 백엔드 개발 회원 서비스개발 파트 3분30초

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,27 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+    * 회원 가입
+    */
+    public Long join(Member member) {
+        //같은 이름이 있는 중복 회원X
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 조재하는 회원입니다.");
+        });
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+}
